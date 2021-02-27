be957a2ccdeb2c8d68c8666e7c5d9bb358530961
Jeferson Costa <rc.jeferson@gmail.com>
2021-02-27 16:33:45 -0300

    feat(survey): adiciona survey
    Adiciona recursos necessários para gerenciamento de surveys.
    
    Adiciona funções de criação e listagem de surveys.

--- api/src/router.ts
+++ api/src/router.ts
@@ -1,10 +1,17 @@
 import { Router } from "express";
+import { SurveyController } from "./controllers/SurveyController";
 import { UserController } from "./controllers/UserController";
 
 const router = Router();
 
 const userController = new UserController();
+const surveyController = new SurveyController();
 
+// Users
 router.post("/users", userController.create);
+
+// Surveys
+router.post("/surveys", surveyController.create);
+router.get("/surveys", surveyController.show);
 
 export { router };
