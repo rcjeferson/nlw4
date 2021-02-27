34a4250e935ee935825e1404b700a9618b341e2c
Jeferson Costa <rc.jeferson@gmail.com>
2021-02-27 15:42:05 -0300

    feat(user): adiciona cadastro de usuário
    Adiciona recursos necessários para permitir o cadastro de usuários
    através da rota /users.

---
+++ api/src/controllers/UserController.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { UsersRepository } from "../repositories/UsersRepository";
+
+class UserController {
+  async create(request: Request, response: Response) {
+    const { name, email } = request.body;
+
+    const userRepository = getCustomRepository(UsersRepository);
+
+    const userAlreadyExists = await userRepository.findOne({
+      email
+    });
+
+    if (userAlreadyExists) {
+      return response.status(400).json({
+        error: `User ${email} already exists!`
+      });
+    }
+
+    const user = userRepository.create({
+      name, email
+    });
+
+    await userRepository.save(user);
+
+    return response.json(user);
+  }
+}
+
+export { UserController };
