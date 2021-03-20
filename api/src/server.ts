4bb0e2b339018d0b6a4fdd347a354ddd946822f1
Jeferson Costa <rc.jeferson@gmail.com>
2021-03-20 16:18:59 -0300

    chore(server): adiciona base_url
    Adiciona BASE_URL como variável de ambiente e configura o servidor para
    identificar e setar corretamente a porta utilizada.
    
    Dessa forma, quando for indicada um porta no BASE_URL o servidor a
    utilizará e quando não for indicado, o servidor vai tentar iniciar
    utilizando a porta http padrão (80).
    
    Com isso, a variável de ambiente BASE_URL ficará disponível para ser
    utilizada dentro do projeto.

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,5 +1,12 @@
 import { app } from "./app";
 
-app.listen(3333, () => {
-  console.log("Server is running on http://localhost:3333");
+let base_url = process.env.BASE_URL
+let port = Number(base_url.split(':')[2])
+
+if (!port) {
+  port = 80
+}
+
+app.listen(port, () => {
+  console.log(`Server is running on ${base_url}`);
 });
